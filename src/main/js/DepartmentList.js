bf0d0839d9c72bd115d8259b4737b5e572a18b19
andrei-mihaicanuta <andrei.canuta@metro.digital>
2022-08-22 12:53:38 +0300

    Conection to frontend

---
+++ src/main/js/DepartmentList.js
@@ -0,0 +1,22 @@
+const { Component } = require("react");
+import Department from './Department';
+
+
+export default class DepartmentList extends Component{
+	render() {
+		const departments = this.props.departments.map(department =>
+			<Department key={department.name} department={department}/>
+		);
+		return (
+			<table>
+				<tbody>
+					<tr>
+						<th>Name</th>
+						<th>Description</th>
+					</tr>
+					{departments}
+				</tbody>
+			</table>
+		)
+	}
+}
--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,4 +1,28 @@
+const { Component } = require('react');
 const React = require('react');
 const ReactDOM = require('react-dom');
+import DepartmentList from './DepartmentList'
 
 console.log("here");
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {departments: []};
+	}
+
+	componentDidMount() {
+		client({method: 'GET', path: '/api/department/all'}).done(response => {
+			this.setState({departments: response});
+		});
+	}
+
+	render() {
+		return (
+		    <div>DepartmentList
+			    <DepartmentList departments={this.state.departments}/>
+			</div>
+		)
+	}
+}
+
